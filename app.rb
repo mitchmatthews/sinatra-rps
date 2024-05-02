9ad378bbb78ad21f7cb22ac5b075fe56583346ad
Mitch Matthews <165244888+mitchmatthews@users.noreply.github.com>
2024-05-02 01:12:36 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -7,3 +7,28 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+  
+  @comp_move = moves.sample
+  
+  if comp_move == "rock"
+    @outcome = "tied"
+  elsif comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  
+  erb(:rock)
+  
+end
+
+get("/scissors") do
+  "This string will be sent as the body of the response"
+end
+
+get("/paper") do
+  "This string will be sent as the body of the response"
+end
